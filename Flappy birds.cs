5f668ced33a6ab22c76bedcdb3d2fc19d5d24f8f
Amirirun <N.Amiri.inf24@stud.bbbaden.ch>
2024-10-18 14:24:55 +0200

    Add files via upload

--- Flappy birds.cs
+++ Flappy birds.cs
@@ -3,115 +3,110 @@ using System.Threading;
 
 class Program
 {
-    static int vogelPosition;
-    static int hindernisPosition;
-    static int hindernisHoehe;
-    static int punkte;
-    static int spielfeldHoehe = 20;
-    static int spielfeldBreite = 50;
-    static bool spielLaeuft;
+    static int birdPosition;
+    static int obstaclePosition;
+    static int obstacleHeight;
+    static int score;
+    static int fieldHeight = 20;
+    static int fieldWidth = 50;
+    static bool isGameRunning;
 
     static void Main(string[] args)
     {
-        // Initiale Anweisungen
         Console.CursorVisible = false;
-        Console.SetWindowSize(spielfeldBreite, spielfeldHoehe + 2);
-        Console.SetBufferSize(spielfeldBreite, spielfeldHoehe + 2);
+        Console.SetWindowSize(fieldWidth, fieldHeight + 2);
+        Console.SetBufferSize(fieldWidth, fieldHeight + 2);
 
-        // Start des Spiels
-        StartSpiel();
+        // Start the game
+        LaunchGame();
     }
 
-    static void StartSpiel()
+    static void LaunchGame()
     {
-        // Initialisiere die Variablen für ein neues Spiel
-        vogelPosition = spielfeldHoehe / 2; // Vogel startet in der Mitte des Spielfelds
-        hindernisPosition = spielfeldBreite - 1; // Hindernis startet am rechten Rand
-        hindernisHoehe = 5; // Initiale Höhe des Hindernisses
-        punkte = 0;
-        spielLaeuft = true;
+        birdPosition = fieldHeight / 2; // Bird starts in the middle
+        obstaclePosition = fieldWidth - 1; // Obstacle starts at the right edge
+        obstacleHeight = 5; // Initial height of the obstacle
+        score = 0;
+        isGameRunning = true;
 
-        // Starte die Spiel-Schleife
-        while (spielLaeuft)
+        while (isGameRunning)
         {
-            SpielLogik();
-            SpielfeldZeichnen();
-            EingabeVerarbeiten();
-            Thread.Sleep(100); // Geschwindigkeit des Spiels
+            GameLogic();
+            DrawField();
+            ProcessInput();
+            Thread.Sleep(100); // Game speed
         }
 
-        // Nach Game Over automatisch neu starten
+        // After Game Over, restart
         Console.Clear();
-        Console.WriteLine("Game Over! Dein Punktestand: " + punkte);
-        Console.WriteLine("Drücke 'R', um neu zu starten...");
+        Console.WriteLine("Game Over! Your score: " + score);
+        Console.WriteLine("Press 'R' to restart...");
 
-        // Warte auf die Eingabe 'R', um das Spiel neu zu starten
+        // Wait for 'R' key to restart the game
         while (Console.ReadKey(true).Key != ConsoleKey.R)
         {
         }
 
-        // Spiel neustarten
-        StartSpiel();
+        LaunchGame();
     }
 
-    static void SpielLogik()
+    static void GameLogic()
     {
-        // Bewegung der Hindernisse
-        hindernisPosition--;
+        obstaclePosition--;
 
-        // Wenn das Hindernis den linken Rand erreicht, ein neues Hindernis erstellen
-        if (hindernisPosition < 0)
+        if (obstaclePosition < 0)
         {
-            hindernisPosition = spielfeldBreite - 1; // Hindernis erscheint wieder rechts
-            hindernisHoehe = new Random().Next(3, spielfeldHoehe - 3); // Zufällige Höhe des Hindernisses
-            punkte++; // Erhöhe den Punktestand, wenn das Hindernis passiert wurde
+            obstaclePosition = fieldWidth - 1;
+            obstacleHeight = new Random().Next(3, fieldHeight - 3);
+            score++;
         }
 
-        // Schwerkraft (Vogel fällt nach unten)
-        vogelPosition++;
+        birdPosition++;
 
-        // Kollision mit dem Boden oder Hindernis
-        if (vogelPosition >= spielfeldHoehe || (hindernisPosition == 5 && (vogelPosition < hindernisHoehe || vogelPosition > hindernisHoehe + 3)))
+        // Nested if for collision
+        if (birdPosition >= fieldHeight)
         {
-            spielLaeuft = false;
+            isGameRunning = false;
+        }
+        else if (obstaclePosition == 5 && (birdPosition < obstacleHeight || birdPosition > obstacleHeight + 3))
+        {
+            isGameRunning = false;
         }
     }
 
-    static void SpielfeldZeichnen()
+    static void DrawField()
     {
         Console.Clear();
 
-        // Zeichne den Vogel, überprüfe ob die Position gültig ist
-        if (vogelPosition >= 0 && vogelPosition < spielfeldHoehe)
+        if (birdPosition >= 0 && birdPosition < fieldHeight)
         {
-            Console.SetCursorPosition(5, vogelPosition);
-            Console.Write("O"); // Der Vogel wird als "O" dargestellt
+            Console.SetCursorPosition(5, birdPosition);
+            Console.Write("O"); // Bird is "O"
         }
 
-        // Zeichne das Hindernis, überprüfe die Positionen
-        for (int i = 0; i < spielfeldHoehe; i++)
+        for (int i = 0; i < fieldHeight; i++)
         {
-            if ((i < hindernisHoehe || i > hindernisHoehe + 3) && hindernisPosition >= 0 && hindernisPosition < spielfeldBreite)
+            if ((i < obstacleHeight || i > obstacleHeight + 3) && obstaclePosition >= 0 && obstaclePosition < fieldWidth)
             {
-                Console.SetCursorPosition(hindernisPosition, i);
-                Console.Write("|"); // Hindernis wird als "|" dargestellt
+                Console.SetCursorPosition(obstaclePosition, i);
+                Console.Write("|"); // Obstacle is "|"
             }
         }
 
-        // Zeichne den Punktestand
-        Console.SetCursorPosition(0, spielfeldHoehe + 1);
-        Console.Write("Punkte: " + punkte);
+        // Draw score in the top-right corner
+        Console.SetCursorPosition(fieldWidth - 10, 0);
+        Console.Write("Score: " + score);
     }
 
-    static void EingabeVerarbeiten()
+    static void ProcessInput()
     {
-        // Überprüfe, ob eine Taste gedrückt wurde
+        // Process user input
         if (Console.KeyAvailable)
         {
-            ConsoleKeyInfo taste = Console.ReadKey(true);
-            if (taste.Key == ConsoleKey.W)
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Spacebar) // Bird flies up when the spacebar is pressed
             {
-                vogelPosition -= 3; // Vogel fliegt nach oben, wenn "W" gedrückt wird
+                birdPosition -= 3;
             }
         }
     }
